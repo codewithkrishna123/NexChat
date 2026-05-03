72df38a4434287e52d48715fe4a61e101a50acfc
codewithkrishna123 <ankanimanbera@gmail.com>
2026-05-03 15:14:02 +0530

    add core android app files (manifest and activity)

---
+++ app/src/main/java/com/nexchat/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nexchat.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("NexChat Temp Running");
+        setContentView(tv);
+    }
+}
